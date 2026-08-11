ec73a3fa5284c16058b5840065d2d97dc179c02d
Naveenkumar-9-A <naveenkumar.aorbotreks@gmail.com>
2026-08-11 14:09:13 +0530

    Update DestinationDetails.jsx

--- aorbo-frontend/src/pages/DestinationDetails.jsx
+++ aorbo-frontend/src/pages/DestinationDetails.jsx
@@ -11,7 +11,7 @@ export default function DestinationDetails() {
   const [error, setError] = useState(null);
   const [addStatus, setAddStatus] = useState('');
 
-  const BACKEND_URL = 'http://127.0.0.1:8000';
+ const BACKEND_URL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000';
 
   useEffect(() => {
     async function getDestinationDetails() {
